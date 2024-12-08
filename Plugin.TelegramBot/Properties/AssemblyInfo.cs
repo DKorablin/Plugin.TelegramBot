59022f8e27baea21f9e5a9e2e6f97773738a35ab
DKorablin2 <danila.korablin@unity3d.com>
2024-12-08 22:35:23 +0300

    Small changes in AssemblyInfo

--- Plugin.TelegramBot/Properties/AssemblyInfo.cs
+++ Plugin.TelegramBot/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: Guid("680b3e5e-7ea3-4374-a83f-584bb3eced40")]
 [assembly: System.CLSCompliant(true)]
 
-#if NETCOREAPP
+#if NETSTANDARD || NETCOREAPP
 [assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/SAL.Interface.TelegramBot")]
 #else
 
