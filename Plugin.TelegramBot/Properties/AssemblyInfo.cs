541718fff4a83766c41ad13313fdd394ff3f16ac
DKorablin <4739804+DKorablin@users.noreply.github.com>
2026-05-04 12:46:26 +0300

    Removed duplicated attribute, CI/CD improvements

--- Plugin.TelegramBot/Properties/AssemblyInfo.cs
+++ Plugin.TelegramBot/Properties/AssemblyInfo.cs
@@ -5,7 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: System.CLSCompliant(true)]
 
 [assembly: AssemblyDescription("Telegram messenger bot host plugin")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2017-2025")]
 
 //C:\Visual Studio Projects\C#\sal.ProcessingServices\sal.Host\Flatbed.WinService.exe
 //C:\Visual Studio Projects\C#\sal.ProcessingServices\sal.Settings\Flatbed.Dialog.exe
